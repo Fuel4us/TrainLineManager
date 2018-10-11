70f97fa8287eaebcce8983401d7f7d1aba7320c3
Gonçalo Fonseca (1150503) <1150503@isep.ipp.pt>
2018-10-11 01:54:47 +0100

    Initial Commit :tada:

---
+++ src/main/java/main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Gonçalo Fonseca (1150503@isep.ipp.pt)
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("Boas este é o nosso trabalho de esinf");
+    }
+}
