848e4c4b544f282f502db81b5506254dfbd13518
Gonçalo Fonseca (1150503) <1150503@isep.ipp.pt>
2018-10-11 16:20:25 +0100

    Deleted main no need for it and added dependecies :zap:

--- src/main/java/main.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Gonçalo Fonseca (1150503@isep.ipp.pt)
- */
-public class main {
-
-    public static void main(String[] args) {
-        System.out.println("Boas este é o nosso trabalho de esinf");
-    }
-}
